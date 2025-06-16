18844b5e44fbcd843c95816f736d4b765ab32039
unknown <eliekiwoy67@gmail.com>
2025-06-16 15:37:18 +0200

    third commit

--- script/teamProfiles.js
+++ script/teamProfiles.js
@@ -7,38 +7,47 @@ export function loadTeamProfiles() {
 
     // Data profile tim
     const teamProfiles = [
+      // {
+      //   image: "assets/Team/team-1.png",
+      //   name: "Leslie Alexander",
+      //   description:
+      //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.",
+      //   socialLinks: {
+      //     linkedIn: "https://linkedin.com",
+      //     twitter: "https://twitter.com",
+      //     dribbble: "https://dribbble.com",
+      //   },
+      // },
+      // {
+      //   image: "assets/Team/team-2.png",
+      //   name: "Jacob Jones",
+      //   description:
+      //     "Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu.",
+      //   socialLinks: {
+      //     linkedIn: "https://linkedin.com",
+      //     twitter: "https://twitter.com",
+      //     dribbble: "https://dribbble.com",
+      //   },
+      // },
+      // {
+      //   image: "assets/Team/team-3.png",
+      //   name: "Courtney Henry",
+      //   description:
+      //     "Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor.",
+      //   socialLinks: {
+      //     linkedIn: "https://linkedin.com",
+      //     twitter: "https://twitter.com",
+      //     dribbble: "https://dribbble.com",
+      //   },
+      // },
       {
         image: "assets/Team/team-1.png",
-        name: "Leslie Alexander",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.",
-        socialLinks: {
-          linkedIn: "https://linkedin.com",
-          twitter: "https://twitter.com",
-          dribbble: "https://dribbble.com",
-        },
       },
       {
         image: "assets/Team/team-2.png",
-        name: "Jacob Jones",
-        description:
-          "Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu.",
-        socialLinks: {
-          linkedIn: "https://linkedin.com",
-          twitter: "https://twitter.com",
-          dribbble: "https://dribbble.com",
-        },
       },
       {
         image: "assets/Team/team-3.png",
-        name: "Courtney Henry",
-        description:
-          "Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor.",
-        socialLinks: {
-          linkedIn: "https://linkedin.com",
-          twitter: "https://twitter.com",
-          dribbble: "https://dribbble.com",
-        },
       },
     ];
 
