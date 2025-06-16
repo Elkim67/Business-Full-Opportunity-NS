d8904a9c88f9265817d6c813e85d9a13f647a49e
unknown <eliekiwoy67@gmail.com>
2025-06-16 15:39:05 +0200

    fourth commit

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 // main.js
 import { toggleMobileMenu } from "./menuToggle.js";
 // import { loadOurServices } from "./ourServices.js";
-// import { loadTeamProfiles } from "./teamProfiles.js";
+import { loadTeamProfiles } from "./teamProfiles.js";
 
 toggleMobileMenu();
 loadOurServices();
