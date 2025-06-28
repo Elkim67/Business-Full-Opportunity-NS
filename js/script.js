a4c08fb56bfaeabd794ad90beaf0c73516b1c73c
unknown <eliekiwoy67@gmail.com>
2025-06-28 11:56:09 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.getElementById('burger-menu');
+    const navLinks = document.getElementById('nav-links');
+  
+    burger.addEventListener('click', () => {
+      navLinks.classList.toggle('show');
+    });
+  });
